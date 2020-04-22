44076f607f6152601fb191acc6164950abfdd87e
arturFormanchuk <4manchuk@gmail.com>
2020-04-22 21:28:37 +0300

    Updates

---
+++ precache-manifest.0accee8e8ec0472f45ce41dddd43c95c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "15fd01e0acf30ec86d9f9c0e0dfe38a4",
+    "url": "/weather/index.html"
+  },
+  {
+    "revision": "c50c7d84427e3c6b9a59",
+    "url": "/weather/static/css/main.3fa945d8.chunk.css"
+  },
+  {
+    "revision": "5d02ce3a992f20ef077d",
+    "url": "/weather/static/js/2.3dfb0c90.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/weather/static/js/2.3dfb0c90.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c50c7d84427e3c6b9a59",
+    "url": "/weather/static/js/main.6596d745.chunk.js"
+  },
+  {
+    "revision": "08cf2222bde43000caed",
+    "url": "/weather/static/js/runtime-main.c045791e.js"
+  },
+  {
+    "revision": "c672622f61ffbf7b428cc6a2a942a734",
+    "url": "/weather/static/media/backgroundhoriz.c672622f.png"
+  },
+  {
+    "revision": "257f9cf033069fecd89167d5f78823b5",
+    "url": "/weather/static/media/img.257f9cf0.png"
+  }
+]);
